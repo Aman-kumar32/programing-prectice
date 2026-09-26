723d22e7f963ec4877bc24a0b0eb983c2eaddf42
Aman-kumar32 <amankumarat12@gmail.com>
2026-09-26 23:22:05 +0530

    adding array-items.c file

---
+++ C-programing/array-items.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10],n,i;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    printf("Enter the %d elements:\n",n);
+    for(i=0;i<n;i++)
+    {
+        printf("Element %d: ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    printf("The array elements are:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;
+}
